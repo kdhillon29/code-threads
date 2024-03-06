2c023d08d2f1b8c5759bafed88e7bff22aeea828
kanwar Dhillon <kdhillon469@gmail.com>
2024-03-07 09:49:27 +1100

    topproducts & productcard component added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import Hero from "@/components/Hero";
+import Info from "@/components/Info";
+import TopProducts from "@/components/TopProducts";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className=" ">
-      <h2>Tshirts</h2>
+      <Hero />
+      <Info />
+      <TopProducts />
     </main>
   );
 }
