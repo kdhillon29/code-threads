b0e1aea7b43d42418f5ecbcba1a1a6ac40d04f71
kanwar Dhillon <kdhillon469@gmail.com>
2024-03-11 18:58:28 +1100

    home page layout fix

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -6,20 +6,20 @@ import { GiTrophy } from "react-icons/gi";
 const Info = () => {
   return (
     <section className="mt-2 py-5 ">
-      <div className="main-container flex justify-between max-md:grid-cols-2 max-md:gap-x-20 max-md:gap-y-10">
-        <div className="flex items-center gap-2 uppercase text-sm">
+      <div className="main-container grid grid-cols-2  md:grid-cols-4  gap-6">
+        <div className="flex items-center gap-2 uppercase text-sm max-sm:text-xs">
           <BiWorld className="text-3xl" />
           <span>Free Shipping Worldwide</span>
         </div>
-        <div className="flex items-center gap-2 uppercase text-sm">
+        <div className="flex items-center gap-2 uppercase text-sm max-sm:text-xs">
           <FaArrowRotateLeft className="text-3xl" />
           <span>Money Back Guarenteed</span>
         </div>
-        <div className="flex items-center gap-2 uppercase text-sm">
+        <div className="flex items-center gap-2 uppercase text-sm max-sm:text-xs">
           <IoIosLock className="text-3xl" />
           <span>Secure Online Payments</span>
         </div>
-        <div className="flex items-center gap-2 uppercase text-sm">
+        <div className="flex items-center gap-2 uppercase text-sm max-sm:text-xs">
           <GiTrophy className="text-3xl" />
           <span>Best Premium Quality</span>
         </div>
